624fb83f683109f9145af1009d925ccde38d9ff9
Itraa1 <zezjly.ai@gmail.com>
2026-01-19 22:46:06 +0300

    add supabase connection

---
+++ src/database/supabase-connection.ts
@@ -0,0 +1,5 @@
+import {createClient} from '@supabase/supabase-js'
+
+if(process.env.SUPABASE_URL && process.env.SUPABASE_KEY){
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
+}
---
+++ src/test.ts
@@ -0,0 +1,1 @@
+console.log(process.env.supabase_Pass);
